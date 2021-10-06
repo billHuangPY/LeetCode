86e7987e21a3c5b1ec78f65786e126bb0973c069
billHuangPY <bill70410@gmail.com>
2021-10-06 09:34:40 -0700

    Create removecell.py

---
+++ CodeSignal/removecell.py
@@ -0,0 +1,15 @@
+'''
+boards = 
+[
+    ['*','*','*'],
+    ['#','*','*'],
+    ['*','*','*'],
+    ['*','*','.'],
+    ['.','.','.'],
+    ['.','#','#']
+]
+It is a game with gravity. '.' is space, '*' is the figure, '#' is the obstacle on the fixed position.
+How many '#' needed to be removed to let any location of the figure's bottom touch the ground?
+
+For example with the boards above, result is 2. (1,0) and (5,1)
+'''
