a01f8fc5c32c63564ad8e6e6e7f8ea5c18dc9d1b
billHuangPY <bill70410@gmail.com>
2021-10-06 09:37:10 -0700

    Create twoSum.py

---
+++ OtheProblems/twoSum.py
@@ -0,0 +1,39 @@
+class Solution(object):
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        sorted_num = sorted(nums)
+        found = False
+        half = int(target / 2)
+        sorted_ans = [0, 1]
+        for i in range(len(sorted_num)):
+            if sorted_num[i] == half:
+                sorted_ans[0] = i
+                sorted_ans[1] = i + 1
+                break
+            elif sorted_num[i] > half:
+                sorted_ans[0] = i-1
+                sorted_ans[1] = i
+                break
+        
+        while not found and sorted_ans[0] >= 0 and sorted_ans[1] < len(nums):
+            #print(sorted_ans)
+            if sorted_num[sorted_ans[0]] + sorted_num[sorted_ans[1]] > target:
+                sorted_ans[0] -= 1
+            elif sorted_num[sorted_ans[0]] + sorted_num[sorted_ans[1]] < target:
+                sorted_ans[1] += 1
+            else:
+                found = True
+                
+        sorted_ans = [sorted_num[i] for i in sorted_ans]
+        ans = []
+        for i in range(len(nums)):
+            if nums[i] in sorted_ans:
+                ans.append(i)
+                if len(ans) == 2:
+                    break
+        return ans
+                
