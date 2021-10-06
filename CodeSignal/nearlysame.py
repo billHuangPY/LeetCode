98aeabfbdc64f8a03774a952b4a40c65c4e8c569
billHuangPY <bill70410@gmail.com>
2021-10-06 09:34:37 -0700

    Create nearlysame.py

---
+++ CodeSignal/nearlysame.py
@@ -0,0 +1,14 @@
+'''
+Given an array,
+nums = [9, 1, 1, 22, 315, 215, 345]
+Find how many possible pairs of (i,j), such that nums[i] and nums[j] have just one different digit.
+
+For example from the above array:
+(9, 1) is a valid pair
+(1, 1) is not a valid pair because they are actually the same
+(315, 215) is a valid pair
+(215, 345) is not a valid pair because they have two different digits
+(315, 345) is a valid pair
+
+The answer of the example is 4
+'''
