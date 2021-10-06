3436329ce49b1aa537ebdf7c7532476becd7a006
billHuangPY <bill70410@gmail.com>
2021-10-06 09:35:00 -0700

    Create nqueen.py

---
+++ OtheProblems/DFS/nqueen.py
@@ -0,0 +1,8 @@
+class Solution(object):
+    def solveNQueens(self, n):
+        """
+        :type n: int
+        :rtype: List[List[str]]
+        """
+        
+    def createApath(self, row, n, )
