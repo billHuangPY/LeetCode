08d2d6d9194e8903e54f348e75f400421f813784
billHuangPY <bill70410@gmail.com>
2021-10-06 09:34:54 -0700

    Create letterphone#.py

---
+++ OtheProblems/DFS/letterphone#.py
@@ -0,0 +1,20 @@
+class Solution(object):
+    def letterCombinations(self, digits):
+        """
+        :type digits: str
+        :rtype: List[str]
+        """
+        result = []
+        if(digits != None and len(digits) > 0):
+            map = ['', '', 'abc', 'def', 'ghi', 'jkl', 'mno', 'pqrs', 'tuv', 'wxyz']
+            self.dfs(digits, 0, result, '', map)
+        return result
+        
+        
+    def dfs(self, digits, index, result, combination, map):
+        if(index == len(digits)):
+            result.append(combination)
+            return
+        
+        for i in map[int(digits[index])]:
+            self.dfs(digits, index+1, result, combination+i, map)
