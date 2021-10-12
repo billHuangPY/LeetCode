ccef01a00c4f2b738f87fd701be7dc63682814ea
billHuangPY <bill70410@gmail.com>
2021-10-11 17:59:25 -0700

    Create 633.py

---
+++ OtheProblems/2pointer/633.py
@@ -0,0 +1,29 @@
+from math import sqrt
+class Solution(object):
+    def judgeSquareSum(self, c):
+        """
+        :type c: int
+        :rtype: bool
+        """
+        left, right = 0, int(sqrt(c))
+        while left <= right:
+            attempt = left**2 + right**2
+            if attempt == c:
+                return True
+            elif attempt < c:
+                left += 1
+            else:
+                right -= 1
+        return False
+
+    def judgeSquareSum2(self, c):
+        """
+        :type c: int
+        :rtype: bool
+        """
+        for i in range(int(sqrt(c)) + 1):
+            b = sqrt(c - i**2)
+            if b == int(b):
+                return True
+        return False
+
