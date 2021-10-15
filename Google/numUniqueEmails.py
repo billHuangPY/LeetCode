2e21d19a3f1dd854b85a294e7ef18e66c60ab21d
billHuangPY <bill70410@gmail.com>
2021-10-15 11:24:12 -0700

    Update numUniqueEmails.py

---
+++ Google/numUniqueEmails.py
@@ -0,0 +1,22 @@
+class Solution(object):
+    def numUniqueEmails(self, emails):
+        """
+        :type emails: List[str]
+        :rtype: int
+        """
+        output = []
+        for email in emails:
+            localname, domain = email.split('@')
+            localname_out = ''
+            for ch in localname:
+                if ch == '+':
+                    break
+                elif ch == '.':
+                    continue
+                else:
+                    localname_out += ch
+            out = localname_out + '@' + domain
+            if not out in output:
+                output.append(out)
+        
+        return len(output)
