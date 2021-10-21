003f5b093547bf3f8dec5ae184350db57b6db50c
billHuangPY <bill70410@gmail.com>
2021-10-21 13:44:14 -0700

    Create 380.py

---
+++ OtheProblems/380.py
@@ -0,0 +1,43 @@
+import random
+class RandomizedSet(object):
+
+    def __init__(self):
+        self.rset = set([])
+        
+
+    def insert(self, val):
+        """
+        :type val: int
+        :rtype: bool
+        """
+        if not val in self.rset:
+            self.rset.add(val)
+            return True
+        else:
+            return False
+        
+
+    def remove(self, val):
+        """
+        :type val: int
+        :rtype: bool
+        """
+        if val in self.rset:
+            self.rset.remove(val)
+            return True
+        else:
+            return False
+        
+
+    def getRandom(self):
+        """
+        :rtype: int
+        """
+        return random.sample(self.rset, 1)[0]
+
+
+# Your RandomizedSet object will be instantiated and called as such:
+# obj = RandomizedSet()
+# param_1 = obj.insert(val)
+# param_2 = obj.remove(val)
+# param_3 = obj.getRandom()
