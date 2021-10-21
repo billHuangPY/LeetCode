b714fb59b44670fbb6221b1fdfc64781fc1fe62f
billHuangPY <bill70410@gmail.com>
2021-10-21 13:44:23 -0700

    Create 122.py

---
+++ OtheProblems/Greedy/122.py
@@ -0,0 +1,19 @@
+class Solution(object):
+    def maxProfit(self, prices):
+        """
+        :type prices: List[int]
+        :rtype: int
+        """
+        profit = 0
+        max_price = prices[0]
+        min_price = prices[0]
+        
+        for price in prices[1:]:
+            if price < max_price:
+                profit += max_price - min_price
+                min_price = price
+                max_price = price
+            else:
+                max_price = price
+        profit += max_price - min_price
+        return profit
