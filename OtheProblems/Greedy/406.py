2cb9497152929246415d9645f96bbb0502253253
billHuangPY <bill70410@gmail.com>
2021-10-21 13:44:25 -0700

    Create 406.py

---
+++ OtheProblems/Greedy/406.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def reconstructQueue(self, people):
+        """
+        :type people: List[List[int]]
+        :rtype: List[List[int]]
+        """
+        sorted_people = sorted(people, key=lambda x: (-x[0], x[1]))
+        
+        queue = []
+        for i in sorted_people:
+            queue.insert(i[1], i)
+        
+        return queue
