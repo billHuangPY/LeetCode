07e3370d2c4cfd4b77d6541c85665c7ea78d5ecf
billHuangPY <bill70410@gmail.com>
2021-10-21 13:44:21 -0700

    Create 121.py

---
+++ OtheProblems/Greedy/121.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def maxProfit(self, prices):
+        """
+        :type prices: List[int]
+        :rtype: int
+        """
+        min_price = 10**5
+        max_profit = 0
+        for i in prices:
+            if i < min_price:
+                min_price = i
+            elif i - min_price > max_profit:
+                max_profit = i - min_price
+        return max_profit
