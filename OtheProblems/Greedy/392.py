aae1b3bf43c3075e09f4254376e0afdbe036782e
billHuangPY <bill70410@gmail.com>
2021-10-23 12:11:13 -0700

    Create 392.py

---
+++ OtheProblems/Greedy/392.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def isSubsequence(self, s, t):
+        """
+        :type s: str
+        :type t: str
+        :rtype: bool
+        """
+        if len(s) <= 0:
+            return True
+        
+        index_s = 0
+        for i in t:
+            if i == s[index_s]:
+                index_s += 1
+                if index_s == len(s):
+                    return True
+        return False
