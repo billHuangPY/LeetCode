117e209015f46ac8456b047fe653c1ae2f2c89e5
billHuangPY <bill70410@gmail.com>
2021-10-27 17:46:48 -0700

    Create 744.py

---
+++ OtheProblems/BNsrch/744.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def nextGreatestLetter(self, letters, target):
+        """
+        :type letters: List[str]
+        :type target: str
+        :rtype: str
+        """
+        left, right = 0, len(letters)-1
+        while left <= right:
+            p = left + int((right-left)/2)
+            if letters[p] > target:
+                right = p - 1
+            else:
+                left = p + 1
+        return letters[left % len(letters)]
