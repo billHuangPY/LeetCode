03db0d5035bfcecb3757a70acb77c9facb8e508d
billHuangPY <bill70410@gmail.com>
2021-10-27 17:46:37 -0700

    Create 278.py

---
+++ OtheProblems/BNsrch/278.py
@@ -0,0 +1,19 @@
+# The isBadVersion API is already defined for you.
+# @param version, an integer
+# @return a bool
+# def isBadVersion(version):
+
+class Solution(object):
+    def firstBadVersion(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        left, right = 1, n
+        while left <= right:
+            p = left + int((right-left)/2)
+            if isBadVersion(p):
+                right = p - 1
+            else:
+                left = p + 1
+        return left
