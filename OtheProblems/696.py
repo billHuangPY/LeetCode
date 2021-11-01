5afbbb9f55c2395f4409e47ee689c96eeddde1e2
billHuangPY <bill70410@gmail.com>
2021-11-01 12:12:47 -0700

    Create 696.py

---
+++ OtheProblems/696.py
@@ -0,0 +1,21 @@
+class Solution(object):
+    def countBinarySubstrings(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        group = []
+        curr = 1
+        for i in range(1, len(s)):
+            if not s[i] == s[i-1]:
+                group.append(curr)
+                curr = 1
+            else:
+                curr += 1
+        group.append(curr)
+        
+        count = 0
+        for i in range(1, len(group)):
+            count += min(group[i-1], group[i])
+        
+        return count
