151f5941a1705bc70ed3e7c872bd361dcf0e662f
billHuangPY <bill70410@gmail.com>
2021-11-13 14:25:10 -0800

    Create 32.py

---
+++ OtheProblems/DP2/32.py
@@ -0,0 +1,20 @@
+class Solution(object):
+    def longestValidParentheses(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        if len(s) <= 1:
+            return 0
+        
+        dp = [0 for i in s]
+        for i in range(1, len(s)):
+            if s[i-1]+s[i] == '()':
+                dp[i] = 2 + dp[i-2]
+            elif s[i-1]+s[i] == '))' and i - dp[i-1] - 1 >= 0 and s[i - dp[i-1] - 1] == '(':
+                dp[i] = 2 + dp[i-1] + dp[i - dp[i-1] - 2]
+        #print(dp)
+        return max(dp)
+        
+        
+                
