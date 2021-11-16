082e89ee5d0c26c9be07bbaa76eb47d7e7096621
billHuangPY <bill70410@gmail.com>
2021-11-15 23:41:51 -0800

    1115

---
+++ DP_path/1137_trib.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def tribonacci(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        if n == 0:
+            return 0
+        if n <= 2:
+            return 1
+        
+        curr, prev1, prev2, curr_fib = 2, 0, 1, 1
+        while curr < n:
+            prev1, prev2, curr_fib = prev2, curr_fib, prev1+prev2+curr_fib
+            curr += 1
+        return curr_fib
---
+++ DP_path/509_fib.py
@@ -0,0 +1,19 @@
+class Solution(object):
+    def fib(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        if n == 0:
+            return 0
+        if n <= 2:
+            return 1
+        
+        curr, curr_fib, prev_fib = 2, 1, 1
+        
+        while curr < n:
+            tmp = curr_fib + prev_fib
+            prev_fib = curr_fib
+            curr_fib = tmp
+            curr += 1
+        return curr_fib
